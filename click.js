c8c2b3792268ce70553f68da01042f2ff0bd5c5b
Brian C <46527668+Joystickiboy@users.noreply.github.com>
2019-01-09 20:03:28 +0100

    Add files via upload

---
+++ click.js
@@ -0,0 +1,13 @@
+function toggleNavPanel(x){
+	let panel = document.getElementById(x);
+	let navarrow = document.getElementById("navarrow");
+	let maxH="45px";
+	
+	if(panel.style.height == maxH){
+		panel.style.height = "0px";
+		navarrow.innerHTML = "&#9662;";
+	} else {
+		panel.style.height = maxH;
+		navarrow.innerHTML = "&#9652;";
+	}
+}
